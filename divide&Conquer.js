a63b9ee027f99df36a14c82e0edf0f12654f4d6d
Rahul Singh <114183358+Rahul4dev@users.noreply.github.com>
2022-12-26 18:01:56 +0530

    Create divide&Conquer.js

---
+++ divide&Conquer.js
@@ -0,0 +1,3 @@
+// Divide and Conquer : 
+
+// It involve the technique of dividing the data into smaller chunks and do our desired ops. and repeating that op on other later. This technique improves our time complexity.
