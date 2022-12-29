f42dfd7e81c9ea3e86e26be8c6f0382a3cdb06c4
Rahul Singh <114183358+Rahul4dev@users.noreply.github.com>
2022-12-29 14:10:24 +0530

    Create RecusionBasic.js

---
+++ day3/RecusionBasic.js
@@ -0,0 +1,61 @@
+// In simple terms, recusive function calls itself during the execution of the function and thus the fucntion calls itself till it meets the base condition/case. Hence it act like a loop. e.g, 
+
+function countDown(num) {
+  if(num <= 0) {
+    console.log("All Done!");
+    return;
+  }
+  console.log(num);
+  num--;
+  countDown(num);
+}
+
+countDown(5);  // 5 4 3 2 1 All done!
+// i.e, it loop over all the num till the num = 0;
+// we can also write this using for-loop or while loop,
+function countDown(num) {
+  for(let i=num; i>=0 ; i++) {
+      console.log(i);
+  }
+  return "All done!";
+}
+countDown(5);  // 5 4 3 2 1 All done!
+
+
+// So, In Recursion we have to consider two important thing, first, the base case where the function execution ends and the repeatetive input it takes during the function execute itself in call stack.
+
+---------------------------------------------------------------------------------
+
+// Similarly, 
+function sumRange(num) {
+    if(num === 1) return 1;
+    return num + sumRange(num -1);
+}
+sumRange(3); // 6 
+// as 3 + sumRange(2) = 3 + 2 + sumRange(1) = 3 + 2 + 1 = 6  
+// Here base case is if condition where it return that op which does not depend on the function. Second return gives the repetetive input till num reaches to 1.
+
+// Can you tell me the output of 
+sumRange(5);  // ?
+// if you know, it is a summation method. so what is 5 summation ?
+
+----------------------------------------------------------
+
+// Similarly we can write factorial method using Recursion 
+
+function findFactorial(num) {
+  if(num ==== 1 ) return 1;
+  return num*findFactorial(num-1);
+}
+
+findFactorail(3); // 6  Only number which gives same output for sumation and factorial.
+findFactorail(5); // 120
+
+// so what will be the output of..
+findFactorail(7);
+
+// So we have just 3 line code for the fatorial and sumation function
+
+--------------------------------------------------------------------
+
+
