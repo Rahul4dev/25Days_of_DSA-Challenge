243db7001e2c4bd29b27507e259c3d1cb7f0bc69
Rahul Singh <114183358+Rahul4dev@users.noreply.github.com>
2022-12-30 19:49:12 +0530

    Update RecusionBasic.js

--- day3/RecusionBasic.js
+++ day3/RecusionBasic.js
@@ -122,4 +122,33 @@ function collectOdd(arr) {
 }
 
 collectOdd([1,2,3,4,5,6,7,8,9]);
+----------------------------------------------------------------------------------------------------------------------------------------
+// POWER 
+function power(base, exponent){
+    if(exponent === 0) return 1;
+    return base * power(base,exponent-1);
+}
+// FACTORIAL 
+function factorial(x){
+   if (x < 0 ) return 0;
+   if (x <= 1 ) return 1;
+   return x * factorial(x-1);
+}
+// PRODUCT OF ARRAY
+function productOfArray(arr) {
+    if(arr.length === 0) {
+        return 1;
+    }
+    return arr[0] * productOfArray(arr.slice(1));
+}
+// RECURSIVE RANGE 
+function recursiveRange(x){
+   if (x === 0 ) return 0;
+   return x + recursiveRange(x-1);
+}
+// FIBONACCI
+function fib(n){
+    if (n <= 2) return 1;
+    return fib(n-1) + fib(n-2);
+}
 
